33f4677c0f8332a3a0b108ef92b5ed77b936b199
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-01-10 12:56:58 +0100

    Script to predict on dev

---
+++ predict_dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mem=30G
+#SBATCH --time=0-1
+#SBATCH -p gpu --gres=gpu:titanx:1
+#SBATCH --array=1-3
+
+test_path=data/ewt.aug.dev
+save_path=exp/lexical-bert/english$SLURM_ARRAY_TASK_ID
+pred_path=$save_path/ewt.aug.dev$SLURM_ARRAY_TASK_ID
+
+gpu=2
+python -u run.py predict\
+    --gpu=$gpu \
+    --test_path=$test_path \
+    --pred_path=$pred_path \
+    --save_path=$save_path
